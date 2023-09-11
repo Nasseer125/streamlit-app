f3816d4500a50dd23cf8929383571290dbf88ea8
Nas WR <45099982+Nasseer125@users.noreply.github.com>
2023-09-12 01:20:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,25 @@
 
 import streamlit as st
+import joblib
 
-st.title('HELLO NAS')
-st.text('This is my first app Streamlist')
-st.text_input('First Name')
-st.number_input('Pick a number',0,10)
+# Charger le modèle hybride
+hybrid_model = joblib.load("hybrid_model.pkl")
+
+# Interface utilisateur Streamlit
+st.title("Système de recommandation de films")
+
+# Formulaire de saisie utilisateur
+user_id = st.text_input("Entrez l'ID de l'utilisateur:")
+movie_id = st.text_input("Entrez l'ID du film:")
+
+if user_id and movie_id:
+    try:
+        user_id = int(user_id)
+        movie_id = int(movie_id)
+
+        # Faire une recommandation en utilisant le modèle hybride
+        prediction = hybrid_model.estimate(user_id, movie_id)
+
+        st.write(f"La prédiction de la note pour l'utilisateur {user_id} et le film {movie_id} est {prediction:.2f}")
+    except ValueError:
+        st.write("Veuillez entrer des valeurs numériques valides.")
