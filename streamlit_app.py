b86c678c09dec3d92c218b8cc2d8af4144bef218
Nas WR <45099982+Nasseer125@users.noreply.github.com>
2023-09-12 23:22:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,53 @@ import pandas as pd
 import streamlit as st
 import joblib
 
+# Chargement des données (contenant les noms des films, les identifiants de films et les genres)
+data = pd.read_csv('votre_fichier_de_donnees.csv')
+
 # Charger le modèle hybride
 hybrid_model = joblib.load("svdpp_model.pkl")
 
 # Interface utilisateur Streamlit
 st.title("Système de recommandation de films")
 
-# Formulaire de saisie utilisateur
-user_id = st.text_input("Entrez l'ID de l'utilisateur:")
-movie_id = st.text_input("Entrez l'ID du film:")
+# Champ de saisie pour l'identifiant ou le nom d'utilisateur
+user_input = st.text_input('Entrez votre identifiant ou nom d\'utilisateur :')
 
-if user_id and movie_id:
-    try:
-        user_id = int(user_id)
-        movie_id = int(movie_id)
+if user_input:
+    # Faites des recommandations avec le modèle
+    user_id = trouver_id_utilisateur(user_input)  # Fonction pour trouver l'ID de l'utilisateur
+    recommendations = faire_des_recommandations(model, user_id)
 
-        # Faire une recommandation en utilisant le modèle hybride
-        prediction = hybrid_model.estimate(user_id, movie_id)
+    # Correspondance des identifiants de films aux noms de films et aux genres
+    movie_id_to_name = dict(zip(data['movieId'], data['title']))
+    movie_id_to_genre = dict(zip(data['movieId'], data['genres']))
 
-        st.write(f"La prédiction de la note pour l'utilisateur {user_id} et le film {movie_id} est {prediction:.2f}")
-    except ValueError:
-        st.write("Veuillez entrer des valeurs numériques valides.")
+    # Affichage des recommandations de films avec les genres
+    st.header('Films Recommandés avec Genres :')
+    for movie_id in recommendations:
+        st.write(f"Nom du Film : {movie_id_to_name[movie_id]}")
+        st.write(f"Genres : {movie_id_to_genre[movie_id]}")
+        st.write("\n")
+
+
+def generate_recommendation(model, user_id, ratings_df, movies_df, n_items):
+    # Obtenez une liste de tous les identifiants de films à partir de l'ensemble de données
+    movie_ids = df_ratings["movieId"].unique()
+    # Obtenir une liste de tous les films qui ont été regardés par l'utilisateur.
+    movie_ids_user = df_ratings.loc[df_ratings["userId"] == user_id, "movieId"]
+    # Obtenir une liste de tous les films IDS qui n'ont pas été regardés par l'utilisateur
+    movie_ids_to_pred = np.setdiff1d(movie_ids, movie_ids_user)
+ 
+   # Appliquer une note de 4 à toutes les interactions (uniquement pour correspondre au format de l'ensemble de données Surprise)
+    test_set = [[user_id, movie_id, 4] for movie_id in movie_ids_to_pred]
+ 
+   # Prévoir les évaluations et générer des recommandations
+    predictions = model.test(test_set)
+    pred_ratings = np.array([pred.est for pred in predictions])
+    print("Top {0} recommandations d'éléments pour l'utilisateur {1}:".format(n_items, user_id))
+   # Classer les films les plus populaires en fonction des évaluations prédites
+    index_max = (-pred_ratings).argsort()[:n_items]
+    for i in index_max:
+        movie_id = movie_ids_to_pred[i]
+        print(df_movies[df_movies["movieId"]==movie_id]["title"].values[0], pred_ratings[i])
+
